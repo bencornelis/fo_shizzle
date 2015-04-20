7c36da3e04441277077dea9107e7ce9eac46d71a
Ben Cornelis & Jesse James <student@epicodus.com>
2015-04-20 11:03:04 -0700

    Add fourth test to go first and pass.

--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('foshizzle')
 
 describe('String#foshizzle') do
+  it('do nothing if no s chars are present') do
+    expect('method thing'.foshizzle()).to(eq('method thing'))
+  end
+
   it('change s chars to z chars') do
     expect('methods test'.foshizzle()).to(eq('methodz tezt'))
   end
@@ -13,5 +17,5 @@ describe('String#foshizzle') do
   it('does not change the first letter of a string') do
     expect('Susans sample test'.foshizzle()).to(eq('Suzanz sample tezt'))
   end
-  
+
 end
