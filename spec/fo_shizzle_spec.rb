31fa9609aa209a4f52289dca78d1d71f858cdf6a
Ben Cornelis & Jesse James <student@epicodus.com>
2015-04-20 10:38:53 -0700

    Write second test and pass.

--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -5,5 +5,8 @@ describe('String#foshizzle') do
   it('change s chars to z chars') do
     expect('methods test'.foshizzle()).to(eq('methodz tezt'))
   end
-  
+
+  it('does not change a capital s') do
+    expect('Susan'.foshizzle()).to(eq('Suzan'))
+  end
 end
